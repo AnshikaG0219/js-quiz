8b0c4c91cc64c5ee70b02880497bb9a60863f735
Anshika Gautam <anshikagautam130@gmail.com>
2021-10-01 14:37:27 +0530

    options

--- index.js
+++ index.js
@@ -14,26 +14,65 @@ var highScores = {
     score: 3,
 }
 
-
 // array of objects
 var questions = [{
-  question: "Where do I live? ",
-  answer: "Delhi"
-}, {
-  question: "My favorite framework would be? ",
-  answer: "Reactjs"
+  question: "Which character has a twin? ",
+  options: {
+    a: "Monica",
+    b: "Chandler",
+    c: "Phoebe"
+  },
+  answer: "c"
+}, 
+{
+  question: "How many sisters does Joey have? ",
+  options: {
+    a: "8",
+    b: "7",
+    c: "9"
+  },
+  answer: "b"
 },
 {
-  question: "What is my first name? ",
-  answer: "Anshika"
-}];
+  question: "What nickname did Monica’s dad give her? ",
+  options: {
+    a: "Tiny Harmonica",
+    b: "Little Electronica",
+    c: "Little Harmoica"
+  },
+  answer: "c"
+},
+{
+  question: "What's the name of the dancer Joey lived with? ",
+  options: {
+    a: "Janice",
+    b: "Janine",
+    c: "Chandler"
+  },
+  answer: "a"
+},
+{
+  question: "What’s Phoebe’s sister’s name? ",
+  options: {
+    a: "Ariel",
+    b: "Ursula",
+    c: "Janine"
+  },
+  answer: "b"
+},
+];
 
 
 // play function
-function play(question, answer) {
-  var userAnswer = readlineSync.question(question);
+function play(question, answer, options) {
+  console.log(question);
+  for(const key in options){
+    console.log(`${key}  : ${options[key]}`);
+  }
+    
+  var userAnswer = readlineSync.question("Choose you option: ");
 
-  if (userAnswer.toUpperCase() === answer.toUpperCase()) { // branching
+  if (userAnswer.toUpperCase() === answer.toUpperCase()) {
     console.log("right!");
     score = score + 1;
   }
@@ -49,7 +88,7 @@ function play(question, answer) {
 function game(userName) {
   for (var i=0; i<questions.length; i++) {
     var currentQuestion = questions[i];
-    play(currentQuestion.question, currentQuestion.answer)
+    play(currentQuestion.question, currentQuestion.answer, currentQuestion.options)
   }
   showScores(score, userName);
 }
